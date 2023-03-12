bea8c9c76d94bcc419376b6562dac4b1e5a840f6
Nuno-Moreira-Ribeiro <nuno-08@outlook.fr>
2023-03-12 21:22:12 +0100

    tooltip without theme.

---
+++ script.js
@@ -0,0 +1,23 @@
+// Initialiser Tippy.js
+
+tippy('#copyBtn', {
+    content: 'Cliquez pour copier mon mail',
+    arrow: true,
+    trigger: 'mouseenter',
+    placement: 'top-end',
+    duration: 500,
+    });
+
+
+    
+    const textToCopy = "nunomoreiraribeiro-08@outlook.fr"
+    const copyBtn = document.getElementById('copyBtn')
+    copyBtn.addEventListener('click', function() {
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        copyBtn._tippy.setContent('Mail copié !');
+    }, function() {
+        copyBtn._tippy.setContent('Une erreur est survenue lors de la copie du mail.');
+    });
+    });
+
+   
